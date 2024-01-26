d2b978a48a0478784f9d8f814eab86e171245df8
Priyanshu Dev <priyanshudev037@gmail.com>
2024-01-26 17:20:50 +0530

    first version of persons

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const Person = require('./models/person')
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+
+
+
+app.post('/person',async (req,res)=>{
+    try{
+        const data = req.body;
+        const newPerson = new Person(data);
+
+        const response = await newPerson.save();
+        console.log('data saved');
+        res.status(200).json(response);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error:'Internal Server Error'});
+    }
+});
+
+
+app.get('/person',async (req,res)=>{
+    try{
+        const data = await Person.find();
+        console.log('data fetched successfully');
+        res.status(200).json(data);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error:'Internal Server Error'});
+    }
+})
+
+
+app.listen(3000);
